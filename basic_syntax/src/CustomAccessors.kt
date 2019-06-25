dd859959f28f5afd281ea05ecec07951d09bd914
Nickson Kaigi <nickaigi@gmail.com>
2019-06-25 13:07:13 +0300

    done with basic

---
+++ basic_syntax/src/CustomAccessors.kt
@@ -0,0 +1,19 @@
+class Rectange(val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() {
+            return height == width
+        }
+}
+
+/* the property 'isSquare' doesn't need a field to store its value
+ * it has a custom getter with the implementation provided. Its value is
+ * computed every time the property is accessed
+ *
+ * Alternative syntax:
+ *      get() = height == width
+ */
+
+fun main(args: Array<String>) {
+    val rectangle = Rectange(41, 43)
+    println(rectangle.isSquare)
+}
---
+++ basic_syntax/src/geometry/example.kt
@@ -0,0 +1,7 @@
+package geometry
+
+import geometry.createRandomRectangle
+
+fun main(args: Array<String>) {
+    println(createRandomRectangle().isSquare)
+}
