1f1f06a0ebb6cce8cab4395b1dc82c06f56ebfba
Nickson Kaigi <nickaigi@gmail.com>
2019-06-26 16:22:08 +0300

    added the mixOptimized

--- basic_syntax/src/enums.kt
+++ basic_syntax/src/enums.kt
@@ -74,9 +74,18 @@ fun mix(c1: Color, c2: Color) =
             else -> throw Exception("Dirty color")
         }
 
+fun mixOptimized(c1: Color, c2: Color) =
+        when {
+            (c1 == Color.RED && c2 == Color.YELLOW) || (c1 == Color.YELLOW && c2 == Color.RED) -> Color.ORANGE
+            (c1 == Color.YELLOW && c2 == Color.BLUE) || (c1 == Color.BLUE && c2 == Color.YELLOW) -> Color.GREEN
+            (c1 == Color.BLUE && c2 == Color.VIOLET) || (c1 == Color.VIOLET && c2 == Color.BLUE) -> Color.INDIGO
+            else -> throw Exception("Dirty Color")
+        }
+
 fun main(args: Array<String>) {
     println(Color.BLUE.rgb())
     println(getMnemonic(Color.BLUE))
     println(getWarmth(Color.ORANGE))
     println(mix(Color.BLUE, Color.YELLOW))
+    println(mixOptimized(Color.BLUE, Color.YELLOW))
 }
