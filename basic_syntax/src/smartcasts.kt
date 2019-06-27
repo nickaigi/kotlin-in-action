3cc5e5adc7abb39dff279ffa5c6dd7be2ab46adc
Nickson Kaigi <nickaigi@gmail.com>
2019-06-27 13:20:06 +0300

    working on this tree data struct

---
+++ basic_syntax/src/smartcasts.kt
@@ -0,0 +1,11 @@
+/*
+* we will write a function that evaluates simple arithmetic expressions like
+* (1 + 2) + 4
+*
+* our example will only include one type of operation: sum of two numbers
+*
+* we store the expression in a tree-like structure where each node is either
+* a sum(Sum) or a number (Num)
+* Num - a leaf node
+* Sum - node has two children
+*  */
