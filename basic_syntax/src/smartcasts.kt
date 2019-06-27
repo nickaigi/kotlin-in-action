f1fe47284de9a475ea03342ace63f5035975c712
Nickson Kaigi <nickaigi@gmail.com>
2019-06-27 18:06:39 +0300

    added a working expr example

--- basic_syntax/src/smartcasts.kt
+++ basic_syntax/src/smartcasts.kt
@@ -1,3 +1,5 @@
+import java.lang.IllegalArgumentException
+
 /*
 * we will write a function that evaluates simple arithmetic expressions like
 * (1 + 2) + 4
@@ -8,4 +10,39 @@
 * a sum(Sum) or a number (Num)
 * Num - a leaf node
 * Sum - node has two children
+*
+* we use an interface called 'Expr' and two classes 'Num' and 'Sum' to
+* implement this
+*
+* The interface doesn't declare any methods.
+* To mark a class implements an interface, use color (:) followed by interface
+* name
 *  */
+
+interface Expr
+class Num(val value: Int): Expr
+
+// the arg of a Sum operation can be any Expr, either Num or another Sum
+class Sum(val left: Expr, val right: Expr) : Expr
+
+/* to store the expression (1+2) + 4
+* Sum(
+*     Sum(
+*         Num(1), Num((2)
+*     ), Num(4)
+* )
+* */
+fun eval(e: Expr): Int {
+    if (e is Num) {
+        val n = e as Num
+        return n.value
+    }
+    if (e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+    throw IllegalArgumentException("Unknown expression")
+}
+
+fun main(args: Array<String>) {
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+}
