9c081e7494b506f3905b0065a279d2269e71f71f
Nickson Kaigi <nickaigi@gmail.com>
2019-06-28 17:51:56 +0300

    diff versions of eval

--- basic_syntax/src/smartcasts.kt
+++ basic_syntax/src/smartcasts.kt
@@ -43,6 +43,54 @@ fun eval(e: Expr): Int {
     throw IllegalArgumentException("Unknown expression")
 }
 
+/* use 'is' to check whether a variable is of a certain type
+* Java uses 'instanceof'
+*
+* But if you've checked that a var has a certain type and needs to access
+* members of that type, you will have to add an explicit cast after the
+* instanceof check
+*
+* In Kotlin, if you check the certain type, you don't need to cast it afterward
+* you can use it as having the type you checked for. The compiler performs the
+* cast for you: 'smart cast'
+* */
+
+/* refactoring the eval function into a more idiomatic Kotlin style */
+
+/* 'if' in Kotlin differs from 'if' in Java *
+ * Java has a ternary operator
+ *
+ *  a > b ? a: b
+ *
+ * Kotlin has no ternary operator, because unlike Java, the 'if' expression
+ * returns a value: thus the expression body syntax
+ *
+ */
+
+fun evalIf(e: Expr): Int =
+        if (e is Num) {
+            e.value
+        } else if (e is Sum) {
+            evalIf(e.right) + evalIf(e.left)
+        } else {
+            throw IllegalArgumentException("Unknown expression")
+        }
+/* *curly braces are optional if there's only one expression in an 'if' branch. */
+
+// rewriting eval using 'when'
+
+fun evalWhen(e: Expr): Int =
+        when (e) {
+            is Num ->
+                e.value
+            is Sum ->
+                evalWhen(e.right) + eval(e.left)
+            else ->
+                throw IllegalArgumentException("Unknown expression")
+        }
+
+/* blocks as branches of 'if'
 fun main(args: Array<String>) {
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println(evalIf(Sum(Num(1), Num(2))))
 }
