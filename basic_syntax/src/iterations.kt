8618e4f8ef5b17fa58424e7a001a02afbe9c0488
Nickson Kaigi <nickaigi@gmail.com>
2019-07-01 18:37:11 +0300

    explanation on kotlin ranges

--- basic_syntax/src/iterations.kt
+++ basic_syntax/src/iterations.kt
@@ -1,4 +1,6 @@
 
+/* fizzbuzz: 
+
 
 fun main(args: Array<String>) {
     var count = 1
@@ -12,5 +14,14 @@ fun main(args: Array<String>) {
         count -= 1
     } while (count > 0)
 
-    /* kotlin doesn't bring anything new to the 'while' and 'do while' loops */
+    /* kotlin doesn't bring anything new to the 'while' and 'do while' loops
+    * Kotlin has the 'for-each' loop
+    * */
+
+    /* to define a range in kotlin, use the .. operator
+    *  start..stop
+    *
+    * stop is always part of the range
+    * */
+    val onToTen = 1..10
 }
