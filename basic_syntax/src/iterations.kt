f34ef0dbf721ff19afbb82851dd1f9ee0c16ed16
Nickson Kaigi <nickaigi@gmail.com>
2019-07-02 20:45:04 +0300

    explanation for until

--- basic_syntax/src/iterations.kt
+++ basic_syntax/src/iterations.kt
@@ -11,20 +11,24 @@ fun fizzBuzz(i: Int) = when {
     else -> "$i "
 }
 
-fun main(args: Array<String>) {
-    var count = 1
-    /* while (count < 10) {
-     *     println("count $count")
-     *     count += 1
-     * }
-     * do {
-     *     println("count $count")
-     *     count -= 1
-     * } while (count > 0)
-     *  kotlin doesn't bring anything new to the 'while' and 'do while' loops
+fun exampleOne() {
+    /*  kotlin doesn't bring anything new to the 'while' and 'do while' loops
      *  Kotlin has the 'for-each' loop
      */
 
+    var count = 1
+    while (count < 10) {
+        println("count $count")
+        count += 1
+    }
+
+    do {
+         println("count $count")
+         count -= 1
+    } while (count > 0)
+}
+
+fun exampleTwo() {
     /* to define a range in kotlin, use the .. operator
     *  start..stop
     *
@@ -33,12 +37,30 @@ fun main(args: Array<String>) {
     for (i in 1..100) {
         print(fizzBuzz(i))
     }
+}
 
+fun exampleThree() {
     /* for loop with a step */
     for (i in 100 downTo 1 step 2) {
         print(fizzBuzz(i))
     }
+}
 
+fun exampleFour() {
     /* you can use 'until'
-    * for (x in number until size)*/
+    * for (x in number until size)
+    *
+    * Key difference between 1..10 and 1 until 10 is that
+    *
+    *   - 1..10 includes the stop number
+    *   - 1 until 10 does not include the stop number
+    *
+    * */
+    for (i in 1 until 30) {
+        print(fizzBuzz(i))
+    }
+}
+
+fun main(args: Array<String>) {
+    exampleFour()
 }
