076e85f9040dedc5bcb5c64e5639d8b64847b942
Nickson Kaigi <nickaigi@gmail.com>
2019-07-05 21:02:17 +0300

    done with the chapter

--- basic_syntax/src/Exceptions.kt
+++ basic_syntax/src/Exceptions.kt
@@ -1,4 +1,7 @@
+import java.io.BufferedReader
+import java.io.StringReader
 import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
 
 /* if (condition) {
 *       throw IllegalArgumentException(
@@ -14,6 +17,59 @@ fun exampleOne(percentage: Int) {
         println("Allowed: $percentage")
     }
 }
+
+fun readNumber(reader: BufferedReader): Int? {
+    try {
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        /* possible to return null because the return type is
+        * Int? can be nullable */
+        return null
+    }
+    finally {
+        reader.close()
+    }
+}
+
+fun exampleTwoE() {
+    val reader = BufferedReader(StringReader("239"))
+    println(readNumber(reader))
+}
+
+// using try as an expression
+fun readNUmberExpression(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        return
+    }
+    println(number)
+}
+
+fun exampleThreeE() {
+    val reader = BufferedReader(StringReader("not a number"))
+    readNUmberExpression(reader)
+}
+
+// returning a value in catch
+fun readNumberCatch(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        null  // null is used in case of an exception
+    }
+    println(number)
+}
+
+fun exampleFourE() {
+    val reader = BufferedReader(StringReader("not a number"))
+    readNumberCatch(reader)
+}
+
 fun main(args: Array<String>) {
-    println(exampleOne(30))
+    //println(exampleOne(30))
+    //exampleTwoE()
+    //exampleThreeE()
+    exampleFourE()
 }
