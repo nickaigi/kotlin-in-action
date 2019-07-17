bda38028f39055bfac6c687434dbb3aa54f4568a
Nickson Kaigi <nickaigi@gmail.com>
2019-07-17 12:57:49 +0300

    initial import

---
+++ src/ch03/3.4.kt
@@ -0,0 +1,16 @@
+package ch03
+
+/* working with collections: varargs, infix calls, and library support
+* - 'vararg' allows you to declare a function taking an arbitrary number of
+*    arguments
+*
+* - an 'infix' notation lets you call some one-argument functions without
+*   ceremony
+*
+* - 'Destructuring declaraitons allow you to unpack a single composite value
+*    into multiple variables
+* */
+
+fun main (args: Array<String>) {
+    /* do the things */
+}
