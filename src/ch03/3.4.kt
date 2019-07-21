e5068e74835c8597bf3e990ebfbbef466d0a8b92
Nickson Kaigi <nickaigi@gmail.com>
2019-07-21 11:44:59 +0300

    added vararg explanatin

--- src/ch03/3.4.kt
+++ src/ch03/3.4.kt
@@ -12,14 +12,34 @@ package ch03
 * */
 
 fun exampleOne3_4(){
+    /* extending the Java Collections API */
     val strings: List<String> = listOf("first", "second", "fourteenth")
     println(strings.last())  //  fourteenth
 
     val numbers: Collection<Int> = setOf(1, 14, 2)
     println(numbers.max())  // 14
+    /* Why is it possible to do so many things with collections in Kotlin even
+    * though they're instances of the Java library classes?
+    * Extension Functions*/
+}
+
+fun exampleTwo3_4() {
+    /* to create a list, we can pass any number of arguments to it */
+    val list = listOf(2, 3, 5, 7, 11)
+
+    /* Java has 'varargs': a feature that allows you to pass an arbitrary
+    * number of values to a method by packing them in an array*/
+
+    /*While in Java we pass the array as is, Kotlin requires you to explicitly
+    * unpack the array, so that every array element becomes a separate argument
+    * to the function
+    *
+    * we use a 'spsread operator'
+    * just put a * in front of the arg*/
 }
 
 fun main (args: Array<String>) {
     /* do the things */
-    exampleOne3_4()
+    val list = listOf("args: ", *args)
+    println(list)
 }
