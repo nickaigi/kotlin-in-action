2e243932f2646ea7c69b2fa9492fa82ebae87ac7
Nickson Kaigi <nickaigi@gmail.com>
2019-07-26 18:32:23 +0300

    i know extension functions

---
+++ src/ch02/extensionFunctions.kt
@@ -0,0 +1,16 @@
+package ch02
+
+fun main(args: Array<String>) {
+    println(changeSpacesToUnderscores("this is my text"))
+    println("this is my text".convertSpacesToUnderscores())
+}
+
+/* what if we wanted to replace all spaces in a string to underscores? We could use a function*/
+fun changeSpacesToUnderscores(str: String): String {
+    return str.replace(" ", "_")
+}
+
+/*What if we wanted that method to be part of the String class?*/
+fun String.convertSpacesToUnderscores(): String {
+    return this.replace(" ", "_")
+}
