78085a9dbcfeaee2b43a52795243a67a37ca9d80
Nickson Kaigi <nickaigi@gmail.com>
2019-08-04 10:05:05 +0300

    expanding range expr, started on smartcasts

--- src/nickson/RangeExpressions.kt
+++ src/nickson/RangeExpressions.kt
@@ -7,25 +7,34 @@ fun main() {
         print(i)
     }
 
-    // reverse
+    /* reverse */
     println("\nReversed using downTo function")
     for (i in 4 downTo 1) print(i)
 
-    // using a step
+    /* using a step */
     println("\nUsing a step function")
     for (i in 1..8 step 2) print(i)
 
-    //OR
+    /* OR */
     println("\nReversed using downTo and step")
     for (i in 8 downTo 1 step 2) print(i)
 
-    // until, excludes the upper limit
+    /* until, excludes the upper limit */
     println("\nUsing until function")
     for (i in 1 until 10) print(i)
 
-    // can be used in an if statement to check if a value belongs in a range
+    /* can be used in an if statement to check if a value belongs in a range */
     println("\nUsed to check if a value is within a range")
-    if (3 in 1..10) {
-        println("Yes")
-    }
+    if (3 in 1..10) println("Yes")
+
+    /* range operator can also be used with characters */
+    println("Characters")
+    for (ch in 'a'..'f') print(ch)
+
+    /* example wit downTo */
+    for (ch in 'f' downTo 'a') print(ch)
+
+    /* Reversed: why would I ever need this? */
+    println("\nReversed")
+    (1..9).reversed().forEach(::print)
 }
