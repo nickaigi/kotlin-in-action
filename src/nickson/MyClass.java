e1a0545cc151e4e2fdbc5d11c82866ddfb6dac64
Nickson Kaigi <nickaigi@gmail.com>
2019-08-07 09:58:56 +0300

    tutorial on interfaces

---
+++ src/nickson/MyClass.java
@@ -0,0 +1,16 @@
+package nickson;
+
+public class MyClass implements MyInterface {
+    @Override
+    public void display() {
+        System.out.println("Geeks");
+    }
+
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+
+        myClass.display();
+
+        System.out.println(a);
+    }
+}
---
+++ src/nickson/MyInterface.java
@@ -0,0 +1,7 @@
+package nickson;
+
+interface MyInterface {
+    final int a = 10; // public, static and final
+
+    void display(); // public and abstract
+}
