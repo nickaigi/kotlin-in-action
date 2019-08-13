64772b44655e4df2561f48b95628c32dc54fb4e9
Nickson Kaigi <nickaigi@gmail.com>
2019-08-13 12:20:20 +0300

    lambda returns

--- src/nickson/OtherLambdas.kt
+++ src/nickson/OtherLambdas.kt
@@ -76,4 +76,14 @@ fun main() {
      *
      * - The above literal is of type '(it: Int) -> Boolean'
      * */
+    val ints = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    ints.filter {
+        val shouldFilter = it % 2 == 0
+        shouldFilter
+    }
+
+    // is equivalent to
+    ints.filter {
+        return@filter it % 2 == 0
+    }
 }
