6ce2f2d91bbc5d78d478183676357b17ab95ab6f
Nickson Kaigi <nickaigi@gmail.com>
2019-08-23 20:19:14 +0300

    2nd diff

--- src/nickson/AbstractVsInterfaces.kt
+++ src/nickson/AbstractVsInterfaces.kt
@@ -2,6 +2,9 @@ package nickson
 
 /* differences between interfaces and abstract classes
  * 1. functions in an interface are Never final.. They can be final in an Abstract class
+ * When we override those functions, we can still use default body using 'super'
+ * 2. Interfaces can have properties
+ * properties in Kotlin represents an abstraction of getter (val) or getter and setter (var)
  * */
 interface Animal {
     // functions in an interface cannot be final, can always be overridden
@@ -10,7 +13,12 @@ interface Animal {
     }
 }
 
-class Fox: Animal
+class Fox: Animal {
+    override fun makeVoice() {
+        super.makeVoice()
+        print(" (I prefer to stay quiet)")
+    }
+}
 
 fun main() {
     val fox = Fox()
