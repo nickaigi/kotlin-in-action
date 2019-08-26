2391735eb3310163932edd1eead1752412a5b755
Nickson Kaigi <nickaigi@gmail.com>
2019-08-26 22:37:31 +0300

    need to take a step back

---
+++ src/nickson/PropertiesAndFields.kt
@@ -0,0 +1,12 @@
+package nickson
+
+/* Declaring properties
+ * - properties in Kotlin can be mutable (var) or read-only (val)
+ * */
+
+class Address {
+    var name: String = "Nickson K"
+    var street: String = "Luthuli Ave"
+    var city: String = "Nairobi"
+    var county: String = "Nairobi"
+}
