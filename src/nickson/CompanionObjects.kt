908efbdcdf5deb7614a6839510e828ddbe669c6d
Nickson Kaigi <nickaigi@gmail.com>
2019-08-26 16:00:28 +0300

    added a static reminder

--- src/nickson/CompanionObjects.kt
+++ src/nickson/CompanionObjects.kt
@@ -1,5 +1,10 @@
 package nickson
 
+/* Companion objects
+ * - Kotlin doesn't have static members/member functions. They recommend you use package-level functions instead.
+ * - Java static members/functions rule of thumb: ask yourself "does it make sense to call this method, even if no Obj
+ * has been constructed yet? If so, it should definitely be static
+ * */
 class EventManager {
     companion object FireBaseManager {}
 }
