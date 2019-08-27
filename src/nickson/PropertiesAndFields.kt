94a10551f5b430b71b17e2c9d9cb7eeb6ed5dc17
Nickson Kaigi <nickaigi@gmail.com>
2019-08-27 18:44:19 +0300

    annotate and change visibility of a property

--- src/nickson/PropertiesAndFields.kt
+++ src/nickson/PropertiesAndFields.kt
@@ -32,17 +32,28 @@ class Bag {
     val size: Int = 0
     val isEmpty: Boolean
         get() = this.size == 0
-    }
+    val isSmall
+        get() = this.size < 10  // inferred type is Boolean
+    var setterVisibility: String = "abc"
+        private set // setter is private, and has default implementation
+    // var setterWithAnnotation: Any? = null
+    //    @Inject set // annotate the setter with Inject
 }
 
 /* using a setter
  * - by convention, the parameter passed to a setter is known as 'value'
+ *
  *      var stringRepresentaion: String
  *          get() = this.toString()
  *          set(value) = {
  *              setDataFromString(value) // parses the string and assigns values to other properties
  *          }
  * */
+
+/* Since Kotlin 1.1 you can omit the property type if it can be inferred from the getter*/
+
+/* If you need to change the visibility of an accessor or to annotate it, but don't need to chane the default
+ * implementation, you can define the accessor without defining its body*/
 
 fun main() {
     var allByDefault: Int?
