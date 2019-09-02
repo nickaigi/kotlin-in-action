6fad191f89e00e5a83bef6a01c650af68bcdba9f
Nickson Kaigi <nickaigi@gmail.com>
2019-09-02 21:41:32 +0300

    visibility modifiers inside a package

--- src/nickson/VisibilityModifiers.kt
+++ src/nickson/VisibilityModifiers.kt
@@ -4,6 +4,24 @@ package nickson
  * - Getters always have the same visibility as the property
  */
 
+/* Packages
+ * Functions, properties and classes, objects and interfaces can be declared on the "top-level", i.e. directly inside a
+ * package:
+ * - if you do not specify any visibility modifier, 'public' is used by default = visible everywhere
+ * - if a declaration is marked as 'private', it will only be visible inside the file containing the declaration.
+ * - if you  mark it 'internal', it is visible everywhere in the same 'module'
+ * - 'protected' is not available for top-level declarations
+ */
+
+private fun myFooFun() {
+    // do things
+} /* visible inside VisibilityModifiers.kt */
+
+public var bar: Int = 5 // property is visible everywhere
+    private set         //setter is visible only in VisibilityModifier.kt
+
+internal val baz = 6
+
 fun main() {
 
 }
