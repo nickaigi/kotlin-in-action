4a1dc356a80d0eed30c37b80bbdd2c328bb32f0c
Nickson Kaigi <nickaigi@gmail.com>
2019-09-02 21:52:08 +0300

    visibility modifiers inside a class

--- src/nickson/VisibilityModifiers.kt
+++ src/nickson/VisibilityModifiers.kt
@@ -22,6 +22,46 @@ public var bar: Int = 5 // property is visible everywhere
 
 internal val baz = 6
 
+/* Classes and Interfaces
+ * For members declared inside a class:
+ * - 'private' means visible inside this class only (including all its members);
+ * - 'protected' same as private + visible in subclasses too;
+ * - 'internal' any client 'inside this module' who sees the declaring class sees its 'internal' members
+ * - 'public' any client who sees the declaring class sees its 'public' members.
+ *
+ * Note: In Kotlin, outer class does not see private members of its inner classes.
+ *
+ * - If you override a 'protected' members and do not specify the visibility explicitly, the overriding member will also
+ * have 'protected' visibility.
+ */
+
+open class Outer {
+    private val a = 1
+    protected open val b = 2
+    internal val c = 3
+    val d = 4  // public by default
+
+    protected class Nested {
+        public val e: Int = 5
+    }
+}
+
+class Subclass : Outer() {
+    // a is not visible
+    // b, c and d are visible
+    // Nested and e are visible
+
+    override val b = 5  // 'b' is protected
+}
+
+class Unrelated(outer: Outer) {
+    // outer.a, outer.b are not visible
+    // outer.c and outer.d are visible (same module)
+    // Outer.Nested is not visible, and Nested::e is not visible either
+}
+
+// TODO: Constructors
+
 fun main() {
 
 }
