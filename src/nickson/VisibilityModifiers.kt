e9700e03ab6e3ea8bb5743789c74c811e1bbe1bf
Nickson Kaigi <nickaigi@gmail.com>
2019-09-02 17:57:32 +0300

    placeholder

---
+++ src/nickson/VisibilityModifiers.kt
@@ -0,0 +1,9 @@
+package nickson
+
+/* Classes, objects, interfaces, constructors, functions, properties and their setters can have visibility modifiers.
+ * - Getters always have the same visibility as the property
+ */
+
+fun main() {
+
+}
