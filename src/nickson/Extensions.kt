13e7df0d3cdbd81f396d28deee957948528a05eb
Nickson Kaigi <nickaigi@gmail.com>
2019-09-03 21:10:37 +0300

    extension functions by the kotlin docs

---
+++ src/nickson/Extensions.kt
@@ -0,0 +1,9 @@
+package nickson
+
+/* Kotlin provides the ability to extend a class with new functionality without having to inherit from the class or use
+ * design patterns such as Decorator.
+ *
+ * You can write new functions for a class from a third-party lib that you can't modify.
+ * Such functions are available for calling in the usual way as if they were methods of the original class.
+ * Known as 'extension functions'
+ */
