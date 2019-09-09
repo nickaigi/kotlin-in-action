f48a4745365a5253339b4da2d9475ae245b54606
Nickson Kaigi <nickaigi@gmail.com>
2019-09-09 09:50:34 +0300

    new topic, Data Classes

---
+++ src/nickson/DataClasses.kt
@@ -0,0 +1,8 @@
+package nickson
+
+/* DATA CLASSES
+ *
+ * - Every time we check for equality using ' == ' operator behind the scenes the ' == ' operator calls a function
+ * named 'equals'
+ * - Every object has an 'equals' function whose implementation determines how the ' == ' operator will behave.
+ */
