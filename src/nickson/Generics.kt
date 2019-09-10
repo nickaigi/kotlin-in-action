e8e9bc2de2a407113fb855082e09457ae434e00d
Nickson Kaigi <nickaigi@gmail.com>
2019-09-10 17:35:59 +0300

    if it can be inferred

--- src/nickson/Generics.kt
+++ src/nickson/Generics.kt
@@ -7,7 +7,18 @@ class Box<T>(t: T) {
     var value = t
 }
 
+/*
+ * to create an instance of 'Box'
+ *
+ *      var box: Box<Int> = Box<Int>(1)
+ * or:
+ *      var box = Box(1)
+ *
+ * if the type can be inferred by the compiler
+ */
+
 fun main() {
-    var b: Box<Int> = Box(2)
+
+    var b = Box(2)
     println(b.value)
 }
