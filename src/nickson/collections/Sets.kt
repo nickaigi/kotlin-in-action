28c9afedd02ac28f8f9ffb46365c2a468a919912
Nickson Kaigi <nickaigi@gmail.com>
2019-09-20 11:25:45 +0300

    duplicates and contains

--- src/nickson/collections/Sets.kt
+++ src/nickson/collections/Sets.kt
@@ -4,6 +4,7 @@ package nickson.collections
  * Sets: for when you need a collection that doesn't allow duplicates.
  * - A set is an ordered collection with no duplicate values.
  * - A set holds only distinct/unique values, duplicates will be ignored.
+ * - Do not have a 'get', you can use '.contains' to check for cardinality
  */
 
 fun printSet(set: Collection<String>) {
@@ -16,4 +17,7 @@ fun printSet(set: Collection<String>) {
 fun main() {
     val friendSet = setOf("Jim", "Sue", "Nick")
     printSet(friendSet)
+
+    val duplicateFriendSet = setOf("Jim", "Sue", "Nick", "Jim")
+    printSet(duplicateFriendSet)  // Jim, Sue, Nick
 }
