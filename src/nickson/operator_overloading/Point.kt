9097930ead0275b4ad2177bc7f54b6e2c371411c
Nickson Kaigi <nickaigi@gmail.com>
2019-10-08 10:11:58 +0300

    added unary operator examples

--- src/nickson/operator_overloading/Point.kt
+++ src/nickson/operator_overloading/Point.kt
@@ -3,7 +3,12 @@ package nickson.operator_overloading
 /* Demo of what I have learned
  */
 
-class Point(var x: Int, var y: Int)
+data class Point(var x: Int, var y: Int) {
+    override fun toString(): String {
+        println("x: $x, y: $y")
+        return super.toString()
+    }
+}
 
 operator fun Point.plus(other: Point): Point {
     return Point(x + other.x, y + other.y)
@@ -17,7 +22,18 @@ operator fun Point.times(scale: Int): Point {
     return Point(x * scale, y * scale)
 }
 
+// unary operators
+operator fun Point.inc() = Point(++x,  ++y)
+operator fun Point.dec() = Point(--x,  --y)
 
 fun main() {
+    val pa = Point(1, 2)
+    var pb = pa * 3
 
+    println(pb)
+    println(pb - pa)
+    println(pb + pa)
+    println("Before increment: $pb")
+    ++pb
+    println("After increment: $pb")
 }
