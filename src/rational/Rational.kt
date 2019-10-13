49720ba507cfe84e05f632224c1b876e4f51ba8f
Nickson Kaigi <nickaigi@gmail.com>
2019-10-13 18:00:28 +0300

    fleshed out rationals according to svtk

--- src/rational/Rational.kt
+++ src/rational/Rational.kt
@@ -1,9 +1,18 @@
 package rational
 
+import java.lang.IllegalArgumentException
 import java.math.BigInteger
 
+
+/* if your class implements comparable, you don't need to create the separate class Rational range
+ * Belonging to range automatically works if your class implements comparable.
+ */
 data class Rational
-private constructor(val numer: BigInteger, val denom: BigInteger){
+private constructor(val numer: BigInteger, val denom: BigInteger): Comparable<Rational>{
+    override fun compareTo(other: Rational): Int {
+        return (numer * other.denom - other.numer * denom).signum()
+    }
+
     companion object{
         fun create(n: BigInteger, d:BigInteger): Rational = normalize(n,d)
 
@@ -13,4 +22,40 @@ private constructor(val numer: BigInteger, val denom: BigInteger){
             return Rational(n / g * sign, d / g * sign)
         }
     }
+
+    operator fun unaryMinus(): Rational = create(-numer, denom)
+
+    operator fun plus(other: Rational) = create(
+        numer * other.denom + other.numer * denom,
+        denom * other.denom
+    )
+
+    operator fun minus(other: Rational): Rational = create(
+        numer * other.denom - other.numer * denom,
+        denom * other.denom
+    )
+
+    operator fun times(other: Rational) =
+        create(numer * other.numer, denom * other.denom)
+
+    operator fun div(other: Rational) =
+        create(numer * other.denom, denom * other.numer)
+
+}
+
+fun String.toRational(): Rational {
+    val message = "Expecting rational in the form of 'n/d' or 'n' was: $this"
+    if (!contains("/")) {
+        val number = toBigIntegerOrNull()
+            ?: throw IllegalArgumentException(message)
+        return Rational.create(number, BigInteger.ONE)
+    }
+
+    val (numerText, denomText) = split("/")
+    val numer = numerText.toBigIntegerOrNull()
+        ?: throw IllegalArgumentException(message)
+
+    val denom = denomText.toBigIntegerOrNull()
+        ?: throw IllegalArgumentException(message)
+    return Rational.create(numer, denom)
 }
