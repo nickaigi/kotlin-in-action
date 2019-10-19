90010afa4bc55ebe50ffc3fb80532dea1999f8b4
Nickson Kaigi <nickaigi@gmail.com>
2019-10-19 10:11:29 +0300

    first draft of BordImp

--- src/board/BoardImpl.kt
+++ src/board/BoardImpl.kt
@@ -1,64 +1,90 @@
 package board
 import board.Direction.*
+import java.lang.IllegalArgumentException
 
 open class SquareBoardImpl(override val width: Int) : SquareBoard {
-    val cells = mutableListOf<Cell>()
+    private val cells = mutableMapOf<Int, List<Cell>>()
+
     init {
+        val rowCells = mutableListOf<Cell>()
         for (row in 1..width) {
             for (column in 1..width)
-                cells.add(Cell(row, column))
+                rowCells.add(Cell(row, column))
+            cells[row] = rowCells
         }
     }
 
-    override fun getCellOrNull(i: Int, j: Int): Cell? {
-
-    }
+    override fun getCellOrNull(i: Int, j: Int): Cell? = getCell(i, j)
 
     override fun getCell(i: Int, j: Int): Cell {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        require(i <= width)
+
+        val rowCells = cells[i]
+
+        return rowCells!![j-1]
     }
 
     override fun getAllCells(): Collection<Cell> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val allCells = mutableListOf<Cell>()
+        for ( (_, rowCells) in cells){
+            for (cell in rowCells)
+                allCells.add(cell)
+        }
+        return allCells
     }
 
     override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val rowCells = cells[i]
+        val result = mutableListOf<Cell>()
+        for (j in jRange) {
+            if (j < width)
+                result.add(rowCells!![j - 1])
+        }
+        return result
     }
 
     override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val columnCells = mutableListOf<Cell>()
+        val result = mutableListOf<Cell>()
+        for (i in iRange) {
+            val rowCells = cells[i]
+            columnCells.add(rowCells!![j -1])
+        }
+
+        return result
     }
 
-    override fun Cell.getNeighbour(direction: Direction): Cell? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun Cell.getNeighbour(direction: Direction): Cell? = when (direction){
+        UP -> getCellOrNull(i - 1, j)
+        DOWN -> getCellOrNull(i + 1, j)
+        RIGHT -> getCellOrNull(i, j + 1)
+        LEFT -> getCellOrNull(i, j - 1)
     }
-
 }
 
 class GameBoardImpl<T>(override val width: Int) : SquareBoardImpl(width), GameBoard<T> {
     override fun get(cell: Cell): T? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override fun set(cell: Cell, value: T?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override fun filter(predicate: (T?) -> Boolean): Collection<Cell> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override fun find(predicate: (T?) -> Boolean): Cell? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override fun any(predicate: (T?) -> Boolean): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override fun all(predicate: (T?) -> Boolean): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
 }
