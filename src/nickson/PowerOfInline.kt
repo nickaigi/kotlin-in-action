8f5e646163cdfb6224b59885d18a9b85ebf9233b
Nickson Kaigi <nickaigi@gmail.com>
2019-10-31 09:22:01 +0300

    added the run2 function that makes use of inline

--- src/nickson/PowerOfInline.kt
+++ src/nickson/PowerOfInline.kt
@@ -2,7 +2,18 @@ package nickson
 
 fun myRun(f:()-> Unit) = f()
 
+/* if we inline the run function */
+inline fun <R> run2(block:() -> R): R = block()
+
+
 fun main() {
     val name = "Kotlin"
+    // under the hood, the compiler creates an anonymous class resulting in additional overhead.
+    // it would have been better to just have the statement
+    // println("Hi, $name!")
+    // Using inline, would not result in overhead.
     myRun { println("Hi, $name!") }
+
+    //using inline, no overhead
+    run2 { println("Hi, $name!") }
 }
