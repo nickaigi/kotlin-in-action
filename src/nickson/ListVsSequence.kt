a62f45c1c7f09f70e295501b2be2d902091159a5
Nickson Kaigi <nickaigi@gmail.com>
2019-11-03 21:00:59 +0300

    sequences vs collections

--- src/nickson/ListVsSequence.kt
+++ src/nickson/ListVsSequence.kt
@@ -7,6 +7,17 @@ package nickson
  *
  * To avoid this, one can make use of sequences, that make use of lazy computation
  */
+
+fun m(i: Int): Int {
+    print("m$i ")
+    return i
+}
+
+fun f(i: Int): Boolean {
+    print("f$i ")
+    return i.rem(2) == 0
+}
+
 fun main() {
     val list = listOf(1, 2, -3)
     val maxOddSquare = list
@@ -23,4 +34,19 @@ fun main() {
         .map { it * it }
         .filter { it.rem(2) == 1 }
         .max()
+
+    // .find returns the first element matching the given predicate or null ir no such element was found
+    val list3 = listOf(1, 2, 3, 4)
+        .map { it * it }
+        .find { it > 3 }
+
+    val list4 = listOf(1, 2, 3, 4)
+    // what is printed?
+    list4.map(::m).filter(::f) // m1 m2 m3 m4 f1 f2 f3 f4
+
+    println("\nUsing asSequence()")
+    list4.asSequence().map(::m).filter(::f).toList()
+
+    println("\nUsing asSequence(): nothing is printed if we don't invoke a result call")
+    list4.asSequence().map(::m).filter(::f)
 }
