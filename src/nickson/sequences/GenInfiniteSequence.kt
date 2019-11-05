a3cba6c8c6baad35c5e4ad8c9bdaaf00fa17de2a
Nickson Kaigi <nickaigi@gmail.com>
2019-11-05 08:26:22 +0300

    how to avoid integer overflow, demo of lazy evaluation on sequences

--- src/nickson/sequences/GenInfiniteSequence.kt
+++ src/nickson/sequences/GenInfiniteSequence.kt
@@ -1,6 +1,20 @@
 package nickson.sequences
 
+import java.math.BigInteger
+
 fun main() {
     val numbers = generateSequence(0) { it + 1 }
     println(numbers.take(5).toList())
+
+    // to prevent integer overflow, use BigInteger
+    val bigNums = generateSequence(BigInteger.ZERO) {
+        it + BigInteger.ONE
+    }
+
+    // how many times will we print the phrase "Generating element"
+    val numbers2 = generateSequence(3) { n ->
+        println("Generating element...")
+        (n + 1).takeIf { it < 7 }
+    }
+    println(numbers2.first())
 }
