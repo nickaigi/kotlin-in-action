cc273fed2889abfc0ccd1b5b5aef11773b952657
Nickson Kaigi <nickaigi@gmail.com>
2019-11-06 20:12:12 +0300

    added more yield statements

--- src/nickson/sequences/YieldExample.kt
+++ src/nickson/sequences/YieldExample.kt
@@ -1,5 +1,15 @@
 package nickson.sequences
 
+fun mySequence() = sequence {
+    println("yield one element")
+    yield(1)
+    println("yield a range")
+    yieldAll(3..5)
+    println("yield a list")
+    yieldAll(listOf(7, 9))
+}
+
+
 fun main() {
     val numbers = sequence {
         var x  = 0
@@ -8,4 +18,9 @@ fun main() {
         }
     }
     println(numbers.take(5).toList())
+
+    println(mySequence()
+        .map { it * it }
+        .filter { it > 10 }
+        .take(1)) // take is not a terminal operation! Therefore, "yield ..." will not be printed at all
 }
