a27ab66c24d00b3e41f9c6952d017a2c0453abb6
Nickson Kaigi <nickaigi@gmail.com>
2019-11-09 09:55:33 +0300

    explanation for with

--- src/nickson/lambda_with_receiver/WithFunction.kt
+++ src/nickson/lambda_with_receiver/WithFunction.kt
@@ -11,11 +11,14 @@ fun main() {
 
     // a kotlin programmer should aim to write
     val sb2 = StringBuilder()
+    /* with(object, lambda) is a regular function that takes the lambda as its second arg
+     * - recall: if a lambda is the second arg of a function, we can move it out of the parenthesis in the function
+     *   call */
     with(sb2) {
         appendln("Alphabet2: ")
         for (c in 'a'..'z') {
             append(c)
         }
-        println(toString())
+        println(this.toString()) // 'this' implicit receiver to the lambda
     }
 }
