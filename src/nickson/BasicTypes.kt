720fe789c110d6e81bfbfe3b50a6acfbc303299c
Nickson Kaigi <nickaigi@gmail.com>
2019-11-12 17:36:51 +0300

    show kotlin bytecode, decompile

--- src/nickson/BasicTypes.kt
+++ src/nickson/BasicTypes.kt
@@ -1,2 +1,12 @@
 package nickson
 
+/* press ctrl+shift+a
+ * - type: show kotlin bytecode
+ * - click the 'decompile' button
+ *
+ * The object of this file was for you to see that under the hood, Kotlin doesn't distinguish between primitive types
+ * and reference types.
+ */
+fun fooInt(): Int = 1
+
+fun barInt(): Int? = 1
