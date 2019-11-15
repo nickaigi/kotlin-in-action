385e8046ef4f2821aeaa219a59151a3f95f8b3c0
Nickson Kaigi <nickaigi@gmail.com>
2019-11-15 13:31:30 +0300

    added annotations

--- src/nickson/java_interop/JavaInterOpExample.kt
+++ src/nickson/java_interop/JavaInterOpExample.kt
@@ -3,5 +3,5 @@ package nickson.java_interop
 fun main() {
     val session = Session()
     val description = session.description // note: kotlin says the type of description is String! (platform type)
-    println(description.length)
+    println(description?.length) // after adding the @Nullable annotation, you must use safe access
 }
--- src/nickson/java_interop/Session.java
+++ src/nickson/java_interop/Session.java
@@ -1,6 +1,11 @@
 package nickson.java_interop;
 
+import org.jetbrains.annotations.Nullable;
+
+/* To annotate Java types, add @Nullable and @NotNull
+ */
 public class Session {
+    @Nullable
     public String getDescription(){
         return null;
     }
