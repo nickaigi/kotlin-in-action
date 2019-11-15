221f3b8cf5351ef8f5c0f6b28672f6886aa37e3a
Nickson Kaigi <nickaigi@gmail.com>
2019-11-15 18:23:13 +0300

    added explanation

--- src/nickson/java_interop/JavaInterOpExample.kt
+++ src/nickson/java_interop/JavaInterOpExample.kt
@@ -2,6 +2,8 @@ package nickson.java_interop
 
 fun main() {
     val session = Session()
-    val description = session.description // note: kotlin says the type of description is String! (platform type)
+    val description = session.description
+    // note: kotlin says the type of description is String! (platform type)
+    // but after we added the @Nullable, now the type is String?
     println(description?.length) // after adding the @Nullable annotation, you must use safe access
 }
